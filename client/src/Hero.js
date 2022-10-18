ef87df02978eba4ca03c7b161bebaff0b5b2e8a2
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2022-10-18 15:24:18 +0200

    Fixed logo style

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
-import Logo from "./assets/logo.png";
+import Logo from "./assets/logo.jpeg";
 import "./Hero.css";
 
 function Hero() {
@@ -9,15 +9,18 @@ function Hero() {
     <div className="background">
       <div class="container">
         <div className="px-4 py-5 my-5 text-center">
-          <Image src={Logo} className="d-block mx-auto mb-4"></Image>
+          <Image
+            src={Logo}
+            className="d-block mx-auto mb-4 shadow-lg p-3 mb-5 bg-white rounded"
+          ></Image>
           <h1 className="display-5 fw-bold">Välkommen till ProMeno</h1>
           <h2 className="display-8">Lär dig om din hälsa</h2>
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-4 mt-4">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of type
-              and scrambled it to make a type specimen book.
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
             </p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
               <Button
