fc5ffd1b1c192d83384c961ee77ca3198c0cb5e6
PugsNotDrugs96 <66440635+PugsNotDrugs96@users.noreply.github.com>
2022-10-20 15:52:50 +0200

    småfixat header och navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,61 +1,23 @@
-const Header = () => {
-    return (
-        <header>
-            <div class="px-3 py-2 text-bg-dark">
-            <div class="container">
-                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xmlnsXlink="#bootstrap"></use></svg>
-                </a>
+import "../styles/Header.css"
 
-                <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-                    <li>
-                    <a href="#" class="nav-link text-secondary">
-                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xmlnsXlink="#home"></use></svg>
-                        Home
-                    </a>
+function Header() {
+    return (
+        <nav className="navbar navbar-expand-sm bg-light">
+            <a className="navbar-brand" href="">ProMeno</a>
+            <button className="navbar-toggler d-flex d-sm-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <img className="navbar-toggle-profile-icon" src="/images/person.svg" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Profil</a>
                     </li>
-                    <li>
-                    <a href="#" class="nav-link text-white">
-                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xmlnsXlink="#speedometer2"></use></svg>
-                        Dashboard
-                    </a>
-                    </li>
-                    <li>
-                    <a href="#" class="nav-link text-white">
-                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xmlnsXlink="#table"></use></svg>
-                        Orders
-                    </a>
-                    </li>
-                    <li>
-                    <a href="#" class="nav-link text-white">
-                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xmlnsXlink="#grid"></use></svg>
-                        Products
-                    </a>
-                    </li>
-                    <li>
-                    <a href="#" class="nav-link text-white">
-                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xmlnsXlink="#people-circle"></use></svg>
-                        Customers
-                    </a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Logga ut</a>
                     </li>
                 </ul>
-                </div>
             </div>
-            </div>
-            <div class="px-3 py-2 border-bottom mb-3">
-                <div class="container d-flex flex-wrap justify-content-center">
-                    <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-                        <input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
-                    </form>
-
-                    <div class="text-end">
-                    <button type="button" class="btn btn-light text-dark me-2">Login</button>
-                    <button type="button" class="btn btn-primary">Sign-up</button>
-                    </div>
-                </div>
-            </div>
-        </header>
+        </nav>
     );
 };
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,40 +1,34 @@
+import "../styles/Navbar.css"
+
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="/images/person.svg" /></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider" /></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <nav className="navbar navbar-expand-sm bg-light">
+            <div className="container">
+                <ul className="nav navbar-nav mx-auto mb-1">
+                        <li className="nav-item dropdown col-xs-6">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Symptom
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href='#'>Another another action </a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item dropdown col-xs-6">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Behandlingar
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Another another action </a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
                 </ul>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
             </div>
-        </div>
         </nav>
     );
 };
