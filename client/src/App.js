fb28af1bc8b043388b750e99013a2c5fd9f41910
NimaFaghihi <nimafaghihi99@gmail.com>
2022-10-20 15:26:52 +0200

    startsida
    Co-Authored-By: Qassem <46705041+Qassem97@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./Homepage"
+import Auth from "./Auth"
+import Register from "./Register";
 
 function App() {
   return (
     <div className="App">
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Homepage />} /> 
+        <Route path="/Auth" element={<Auth />} /> 
+        <Route path="/Register" element={<Register />} /> 
+      </Routes>
+    </BrowserRouter>
     </div>
+  
   );
 }
 
