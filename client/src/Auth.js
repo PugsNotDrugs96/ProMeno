2e30826fe8f079486cb1a035d0d7ca6e1b6969f3
Qassem <46705041+Qassem97@users.noreply.github.com>
2022-10-20 23:04:06 +0200

    css for sign in/up

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -53,7 +53,7 @@ export default function (props) {
     <div className="Auth-form-container">
       <form className="Auth-form">
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
+          <h3 className="Auth-form-title">Sign up</h3>
           <div className="text-center">
             Already registered?{" "}
             <span className="link-primary" onClick={changeAuthMode}>
