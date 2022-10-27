2b9f99c462a988ccc08268cdec70813fe02731be
PugsNotDrugs96 <66440635+PugsNotDrugs96@users.noreply.github.com>
2022-10-27 12:06:43 +0200

    uppdaterat registreringssidan

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import Navbar from "./Components/navbar/Navbar";
-import Header from "./Components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import NotLoggedInPage from "./Components/not-logged-in-page/NotLoggedInPage";
-import LoggedInPage from "./Components/logged-in-page/LoggedInPage";
-import Auth from "./Components/not-logged-in-page/Auth";
-import Register from "./Components/not-logged-in-page/Register";
+import NotLoggedInPage from "./components/not-logged-in-page/NotLoggedInPage";
+import LoggedInPage from "./components/logged-in-page/LoggedInPage";
+import Auth from "./components/not-logged-in-page/Auth";
+import Register from "./components/not-logged-in-page/Register";
 
 function App() {
   return (
@@ -17,8 +17,8 @@ function App() {
         <div className="container">
           <BrowserRouter>
             <Routes>
-              <Route path="" element={<NotLoggedInPage />} />
-              <Route path="/home" element={<LoggedInPage />} />
+              <Route path="/" element={<NotLoggedInPage />} />
+              <Route path="/home" element={<LoggedInPage />} /> {/*man ska inte kunna gå in på inloggat läge direkt från URL */}
               <Route path="/Auth" element={<Auth />} />
               <Route path="/Register" element={<Register />} />
             </Routes>
--- client/src/Components/not-logged-in-page/Register.js
+++ client/src/Components/not-logged-in-page/Register.js
@@ -1,47 +1,55 @@
 import React, { useState } from "react";
 
 function Register(props) {
-  let [authMode, setAuthMode] = useState("register");
+  const [message, setMessage] = useState(""); 
+  const [authStep, setAuthMode] = useState("step1");
 
+  const handleChange = event => {
+    setMessage(event.target.value);
+  }
+
+  const handleClick = event => {
+    event.preventDefault();
+
+    if(message === "test123"){
+      setMessage("")
+      setAuthMode("step2");
+    }
+  };
+
+  if(authStep === "step2"){
+    return (
+      <div className="container">
+      <div className="col-md-5 mx-auto col-lg-5">
+        <form className="p-4 p-md-5 border rounded-3 bg-light">
+        <h3 className="text-center">Steg 2</h3>
+          <div className="form-floating mb-3">
+            <input type="email" className="form-control" id="e-post" name="e-post" value={message} placeholder="Skriv e-postadress här.." onChange={handleChange}/>
+            <label for="floatingInput">E-post</label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Registrera</button>
+          <hr className="my-4" />
+          <small className="text-muted">Genom att trycka på knappen registrera så godkänner jag villkoren för att delta i studien.</small>
+        </form>
+      </div>
+    </div>
+    )
+  }
+  
   return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign up</h3>
-          <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="e.g Jane Doe"
-            />
+    <div className="container">
+      <div className="col-md-5 mx-auto col-lg-5">
+        <form className="p-4 p-md-5 border rounded-3 bg-light">
+        <h3 className="text-center">Steg 1</h3>
+          <div className="form-floating mb-3">
+            <input type="text" className="form-control" id="regCode" name="regCode" value={message} placeholder="Skriv registreringskoden här.." onChange={handleChange}/>
+            <label for="floatingInput">Registreringskod</label>
           </div>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="text-center mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
-        </div>
-      </form>
+          <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Nästa</button>
+{/*           <hr className="my-4" />
+          <small className="text-muted">By clicking Sign up, you agree to the terms of use.</small> */}
+        </form>
+      </div>
     </div>
   );
 }
