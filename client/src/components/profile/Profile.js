8852b5d692ff38e0aeff5c79a295011aced1a6c5
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2022-10-27 15:45:03 +0200

    Stylat profil-sida

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 function Profile() {
+    const userEmail = "user@email.com";
+
     return (
-        <div>
-            test
+        <div className="px-4 py-4 my-5 text-center">
+            <h2 className="fw-bold">Profil</h2>
+            <h4 className="fw-light fst-italic mb-5">{userEmail}</h4>
+            <div className="d-grid gap-2">
+                <Button variant="primary" size="lg">
+                    Ändra lösenord
+                </Button>
+                <Button variant="secondary" size="lg">
+                    Avregistrera från studie
+                </Button>
+            </div>
         </div>
     );
 }
