d35c3f9951d8053dbd223a794172ad7236775319
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2022-11-01 10:39:47 +0100

    Fix mainCategories

--- client/src/Components/navbar/Navbar.js
+++ client/src/Components/navbar/Navbar.js
@@ -1,192 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/Navbar.css";
-import {getCategories} from "../../api"
+import { getCategories } from "../../api";
 
 const Navbar = () => {
-  const [toggle1, setToggle1] = useState(false);
-  const [toggle2, setToggle2] = useState(false);
-  const [categories, setCategories] = useState([]);  
-  const [mainCategories, setMainCategories] = useState([]);
-  const [childCategories, setChildCategories] = useState([]);
-    
-  const handleClick1 = () => {
-    if (toggle2) {
-      setToggle2(!toggle2);
-    }
-    setToggle1(!toggle1);
-  };
+  const [categories, setCategories] = useState([]);
 
-/*   const handleClick2 = () => {
-    if (toggle1) {
-      setToggle1(!toggle1);
-    }
-    setToggle2(!toggle2);
-  }; */
-
-     useEffect(() => {
-      async function fetchData() {
-        const categories = await getCategories();
-        for(let i = 0; i < categories.length; i++){
-          if(categories[i].parent === 0) {
-            mainCategories.push(categories[i].name);
-          } else {
-            childCategories.push(categories[i].name);
-          }
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const categories = await getCategories();
       setCategories(categories);
-      console.log(mainCategories.length);
-      }
-      fetchData();
-    }, []) 
+    }
+    fetchData();
+  }, []);
 
-/*   async function fetchData(){
-    const categories = await getCategories();
-    const newThing = categories.map(item =>{
-      if(item.parent === 0) {
-        setMainCategories((prevState) => {
-          return [item.name, ...prevState]
-        })
-      } else {
-        setChildCategories((prevState) => {
-          return [item.name, ...prevState]
-        })
-      }
-    })
-    console.log(mainCategories.length);
-  }
+  if (!categories) return null;
 
-  fetchData(); */
+  const mainCategories = categories.filter((category) => !category.parent);
 
-/*   function getMainCategories(){
-    let mainCat = [];
-    categories.forEach(element => {
-      if(element.parent === 0){
-        mainCat.push(element.name)
-      } else {
-        mainCat.push(element.name)
-      }
-    })
-    return mainCat;
-  } */
+  const childCategories = categories.filter((category) => category.parent);
 
-  console.log(categories)
+  console.log(mainCategories);
 
   return (
     <div className="container">
       <div className="navbar navbar-expand-sm">
         <ul className="navbar-nav mx-auto">
-        {mainCategories.map((item) => (
-          <li className="nav-item category-main">
-            <a
-              className="nav-link dropdown-toggle mx-auto"
-              onClick={handleClick1}
-              href="/"
-              role="button"
-              data-bs-toggle="collapse"
-              aria-expanded="false"
-            >
-              {item}
-            </a>
-          </li>
-        ))}
-          {/* <li className="nav-item category-main">
-            <a
-              className="nav-link dropdown-toggle mx-auto"
-              onClick={handleClick1}
-              href="/"
-              role="button"
-              data-bs-toggle="collapse"
-              aria-expanded="false"
-            >
-              Symptom
-            </a>
-          </li>
-          <li className="nav-item category-main">
-            <a
-              className="nav-link dropdown-toggle mx-auto"
-              onClick={handleClick2}
-              href="/"
-              role="button"
-              data-bs-toggle="collapse"
-              aria-expanded="false"
-            >
-              Behandlingar
-            </a>
-          </li> */}
+          {mainCategories.map((item) => (
+            <li className="nav-item category-main">
+              <a
+                className="nav-link dropdown-toggle mx-auto"
+                href="/"
+                role="button"
+                data-bs-toggle="collapse"
+                aria-expanded="false"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
-      {toggle1 && (
-        <ul className="navbar-nav align-items-center">
-          <div className="navbar navbar-expand-sm">
-            <div className="row">
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Symptom 1
-                  </a>
-                </li>
-              </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Symptom 2
-                  </a>
-                </li>
-              </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Symptom 3
-                  </a>
-                </li>
-              </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Symptom 4
-                  </a>
-                </li>
-              </div>
-            </div>
-          </div>
-        </ul>
-      )}
-      {toggle2 && (
-        <ul className="navbar-nav align-items-center">
-          <div className="navbar navbar-expand-sm">
-            <div className="row">
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Behandling 1
-                  </a>
-                </li>
-              </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Behandling 2
-                  </a>
-                </li>
-              </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Behandling 3
-                  </a>
-                </li>
-              </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <li className="category-second">
-                  <a className="category-second" href="/">
-                    Behandling 4
-                  </a>
-                </li>
-              </div>
-            </div>
-          </div>
-        </ul>
-      )}
     </div>
   );
 };
