67a86dbcef128145b9b9977a673f9c64bedbe798
Qassem <46705041+Qassem97@users.noreply.github.com>
2022-11-08 00:12:28 +0100

    react-bootstrap sign in/up

--- client/src/components/not-logged-in-page/Auth.js
+++ client/src/components/not-logged-in-page/Auth.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import Register from "./Register";
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Auth(props) {
   let [authMode, setAuthMode] = useState("signin");
   const [mail, setMail] = useState(""); 
@@ -30,56 +36,77 @@ function Auth(props) {
   };
 
   if (authMode === "signin") {
-    return (
-      <div className="container ">
-        <div className="col-md-5 mx-auto col-lg-5">
-          <form className="p-4 p-md-5 border rounded-3 bg-light">
-            <h3 className="text-center">Logga in</h3>
-            <div className="text-center">Inte registrerad ännu? {" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Registrera här!
-              </span>
-            </div>
-            <div className="form-floating mb-3">
-            <input type="email" className="form-control"
-            id="e-post" name="e-post" value={mail} 
-            // placeholder="Skriv e-postadress här.."
-            onChange={handleChange}/>
-            <label for="floatingInput">E-post</label>
-            </div>
-            <div className="form-floating mb-3">
-              <input
-                type="password"
-                className="form-control"
-                // placeholder="Ange ditt lösenord"
-                // onChange={handleChange}
-                />
+    // return (
+      // <div className="container ">
+      //   <div className="col-md-5 mx-auto col-lg-5">
+      //     <form className="p-4 p-md-5 border rounded-3 bg-light">
+      //       <h3 className="text-center">Logga in</h3>
+      //       <div className="text-center">Inte registrerad ännu? {" "}
+      //         <span className="link-primary" onClick={changeAuthMode}>
+      //           Registrera här!
+      //         </span>
+      //       </div>
+      //       <div className="form-floating mb-3">
+      //       <input type="email" className="form-control"
+      //       id="e-post" name="e-post" value={mail} 
+      //       // placeholder="Skriv e-postadress här.."
+      //       onChange={handleChange}/>
+      //       <label for="floatingInput">E-post</label>
+      //       </div>
+      //       <div className="form-floating mb-3">
+      //         <input
+      //           type="password"
+      //           className="form-control"
+      //           // placeholder="Ange ditt lösenord"
+      //           // onChange={handleChange}
+      //           />
 
-              <label for="floatingInput">Lösenord</label>
+      //         <label for="floatingInput">Lösenord</label>
 
-            </div>
-            <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Skicka</button>
+      //       </div>
+      //       <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Skicka</button>
 
-            <p className="text-center mt-2">
-              Glömt <a href="/">lösenord?</a>
-            </p>
-        </form>
-        </div>
+      //       <p className="text-center mt-2">
+      //         Glömt <a href="/">lösenord?</a>
+      //       </p>
+      //   </form>
+      //   </div>
         
-      </div>
+      // </div>
+    // );
+
+    return (
+      <Container>
+          <Col> <h1 className="text-center text-info"> Logga in</h1> </Col>
+          <div className="text-center">Inte registrerad ännu? {" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Registrera här!
+            </span>
+          </div>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="col-md-5 mb-3" controlId="formBasicEmail">
+                <Form.Label>E-post adress</Form.Label>
+                <Form.Control type="email" placeholder="Ange ditt mail" />
+              </Form.Group>
+              <Form.Group className="col-md-5 mb-3" controlId="formBasicPassword">
+                <Form.Label>Lösenord</Form.Label>
+                <Form.Control type="password" placeholder="Ange ditt lösenord" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Skicka
+              </Button>
+              <span> Glömt <a href="/">lösenord?</a></span>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 
   return (
-    <div className="container ">     
-      <h3 className="text-center" >Registrera dig</h3>
-      <div className="text-center">Redan registrerad?{"  "}
-        <span className="link-primary" onClick={changeAuthMode}>
-          Logga in
-        </span>
-      </div>
-      <Register/>
-    </div>
+        <Register/>  
   );
 }
 
--- client/src/components/not-logged-in-page/Register.js
+++ client/src/components/not-logged-in-page/Register.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
 
 function Register(props) {
   const [message, setMessage] = useState(""); 
@@ -19,41 +24,82 @@ function Register(props) {
 
   if(authStep === "step2"){
     return (
-      <div className="container">
-      <div className="col-md-5 mx-auto col-lg-5">
-        <form className="p-4 p-md-5 border rounded-3 bg-light">
-        <h3 className="text-center">Steg 2</h3>
-          <div className="form-floating mb-3">
-            <input type="email" className="form-control"
-            id="e-post" name="e-post" value={message} 
-            placeholder="Skriv e-postadress här.."
-            onChange={handleChange}/>
-            <label for="floatingInput">E-post</label>
+    //   <div className="container">
+    //   <div className="col-md-5 mx-auto col-lg-5">
+    //     <form className="p-4 p-md-5 border rounded-3 bg-light">
+    //     <h3 className="text-center">Steg 2</h3>
+    //       <div className="form-floating mb-3">
+    //         <input type="email" className="form-control"
+    //         id="e-post" name="e-post" value={message} 
+    //         placeholder="Skriv e-postadress här.."
+    //         onChange={handleChange}/>
+    //         <label for="floatingInput">E-post</label>
+    //       </div>
+    //       <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Registrera</button>
+    //       <hr className="my-4" />
+    //       <small className="text-muted">Genom att trycka på knappen registrera så godkänner jag villkoren för att delta i studien.</small>
+    //     </form>
+    //   </div>
+    // </div>
+
+    <Container>
+      <Col> <h1 className="text-center text-info"> Registrera dig!</h1> </Col>
+      <Form>
+        <h3 className="col-md-5 mb-3">Steg 2</h3>
+        <Form.Group className="col-md-5 mb-3" controlId="formBasicEmail">
+          <Form.Label>E-post adress</Form.Label>
+          <Form.Control type="email" placeholder="Ange ditt mail här.." onChange={handleChange}/>
+        </Form.Group>    
+          <Button  className="mr-2" variant="primary"  type="submit" onClick={handleClick}>
+            Registrera 
+          </Button>
+          <div>
+            <Form.Text className="text-muted">
+            Genom att trycka på knappen registrera så godkänner jag villkoren för att delta i studien.    
+            </Form.Text>
           </div>
-          <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Registrera</button>
-          <hr className="my-4" />
-          <small className="text-muted">Genom att trycka på knappen registrera så godkänner jag villkoren för att delta i studien.</small>
-        </form>
-      </div>
-    </div>
+      </Form>
+    </Container>
     )
   }
-  
+
   return (
-    <div className="container">
-      <div className="col-md-5 mx-auto col-lg-5">
-        <form className="p-4 p-md-5 border rounded-3 bg-light" >
-        <h3 className="text-center">Steg 1</h3>
-          <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="regCode" name="regCode" value={message} placeholder="Skriv registreringskoden här.." onChange={handleChange}/>
-            <label for="floatingInput">Registreringskod</label>
-          </div>
-          <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Nästa</button>
-{/*           <hr className="my-4" />
-          <small className="text-muted">By clicking Sign up, you agree to the terms of use.</small> */}
-        </form>
-      </div>
-    </div>
+    // <div className="container">
+    //   <div className="col-md-5 mx-auto col-lg-5">
+    //     <form className="p-4 p-md-5 border rounded-3 bg-light" >
+    //     <h3 className="text-center">Steg 1</h3>
+    //       <div className="form-floating mb-3">
+    //         <input type="text" className="form-control" id="regCode" name="regCode" value={message} placeholder="Skriv registreringskoden här.." onChange={handleChange}/>
+    //         <label for="floatingInput">Registreringskod</label>
+    //       </div>
+    //       <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleClick}>Nästa</button>
+    //     </form>
+    //   </div>
+    // </div>
+
+    <Container>
+      <Col> <h1 className="text-center text-info"> Registrera dig!</h1> </Col>
+      <Form>
+        <h3 className="text-center mb-3">Steg 1</h3>
+        <Form.Label>Registreringskod</Form.Label>
+        <Form.Group className="col-md-5 mb-3" controlId="textarea">
+          <Form.Control
+            as="input"
+            size="md"
+            placeholder="Skriv registreringskoden här.." onChange={handleChange}
+          ></Form.Control>
+        </Form.Group>
+        <div>
+          <Button variant="primary"  type="submit" onClick={handleClick}>
+            Nästa 
+          </Button>
+          <Button variant="secondary"  type="submit">
+            Tillbaka
+          </Button>
+        </div>
+      </Form>
+    </Container>
+
   );
 }
 
