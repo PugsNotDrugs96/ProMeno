7a30eff539f63d9f4f50e2a087bd22bab99ad7ef
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2022-12-01 21:04:20 +0100

    Create new file structure

---
+++ client/src/components/post-card-page/PostCard.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Card from "react-bootstrap/Card";
+import cardImg from "../../assets/card-image.png";
+import { useNavigate } from "react-router-dom";
+
+function PostCard(props) {
+  const { index, post } = props;
+  const navigate = useNavigate();
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  return (
+    <Card
+      key={index}
+      style={{
+        width: "14rem",
+        height: "17rem",
+        margin: "0.8rem",
+        cursor: "pointer",
+        opacity: isHover ? 0.8 : 1,
+        backgroundColor: "#ce94ad",
+        color: "white",
+        borderRadius: "15%",
+        marginLeft: "5.5rem",
+        border: "none",
+      }}
+      onClick={() => {
+        navigate(`/post/${post.slug}`);
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Card.Img
+        variant="top"
+        src={post.better_featured_image?.source_url ?? cardImg}
+        style={{
+          width: "100%",
+          maxHeight: "10rem",
+          borderTopLeftRadius: "15%",
+          borderTopRightRadius: "15%",
+        }}
+      />
+      <Card.Body className="text-center">
+        <Card.Title
+          dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+          style={{ fontSize: "20px" }}
+        />
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PostCard;
--- client/src/components/post-card-page/PostCardPage.js
+++ client/src/components/post-card-page/PostCardPage.js
@@ -1,15 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getPostsByCategory, getCategoryBySlug } from "../../api/api";
-import Card from "react-bootstrap/Card";
-import cardImg from "../../assets/card-image.png";
-import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/esm/Col";
+import PostCard from "./PostCard";
 
-function CategoryPage() {
-  const navigate = useNavigate();
+function PostCardPage() {
   const params = useParams();
   const { slug } = params;
 
@@ -51,40 +48,7 @@ function CategoryPage() {
       <Row xs={1} md={2} className="g-3">
         {posts.map((post, index) => (
           <Col key={index} className="d-flex">
-            <Card
-              key={index}
-              style={{
-                width: "14rem",
-                height: "17rem",
-                margin: "0.8rem",
-                cursor: "pointer",
-                backgroundColor: "#ce94ad",
-                color: "white",
-                borderRadius: "15%",
-                marginLeft: "5.5rem",
-                border: "none",
-              }}
-              onClick={() => {
-                navigate(`/post/${post.id}`);
-              }}
-            >
-              <Card.Img
-                variant="top"
-                src={post.better_featured_image?.source_url ?? cardImg}
-                style={{
-                  width: "100%",
-                  maxHeight: "10rem",
-                  borderTopLeftRadius: "15%",
-                  borderTopRightRadius: "15%",
-                }}
-              />
-              <Card.Body className="text-center">
-                <Card.Title
-                  dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-                  style={{ fontSize: "24px" }}
-                />
-              </Card.Body>
-            </Card>
+            <PostCard index={index} post={post} />
           </Col>
         ))}
       </Row>
@@ -92,4 +56,4 @@ function CategoryPage() {
   );
 }
 
-export default CategoryPage;
+export default PostCardPage;
