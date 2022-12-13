0d2006bbda4b70c2805a757d1889b94e50030307
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2022-12-13 21:20:42 +0100

    Style front-page

--- client/src/components/signed-out-page/SignedOutPage.js
+++ client/src/components/signed-out-page/SignedOutPage.js
@@ -4,26 +4,7 @@ import Hero from "../hero/Hero";
 import Button from "react-bootstrap/Button";
 
 function SignedOutPage() {
-  return (
-    <>
-      <Hero />
-      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <Link to="/login">
-          <Button className="btn btn-success btn-lg px-4 gap-3">
-            Logga in
-          </Button>
-        </Link>
-        <Link to="/register">
-          <Button
-            variant="primary"
-            className="btn btn-success btn-lg px-4 gap-3"
-          >
-            Registrera dig
-          </Button>
-        </Link>
-      </div>
-    </>
-  );
+  return <Hero />;
 }
 
 export default SignedOutPage;
