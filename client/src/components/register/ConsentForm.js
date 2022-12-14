7b7c5cb890fde5d956dd78078bce8965172de7fc
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2022-12-14 20:07:28 +0100

    Add loading

--- client/src/components/register/ConsentForm.js
+++ client/src/components/register/ConsentForm.js
@@ -7,11 +7,14 @@ import AlertTitle from "@mui/material/AlertTitle";
 
 function ConsentForm() {
   const [form, setForm] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const form = await getPostBySlug("villkor-for-studien");
       setForm(form);
+      setIsLoading(false);
     }
     fetchData();
   }, []);
@@ -25,20 +28,30 @@ function ConsentForm() {
     );
 
   return (
-    <Card
-      style={{ background: "transparent", border: "none", margin: "0 2rem" }}
-    >
-      <Card.Title
-        className="title"
-        style={{ fontSize: "30px" }}
-        dangerouslySetInnerHTML={{ __html: form.title.rendered }}
-      />
-      <Card.Body
-        className="body"
-        style={{ fontSize: "20px", padding: 0, marginTop: "2rem" }}
-        dangerouslySetInnerHTML={{ __html: form.content.rendered }}
-      />
-    </Card>
+    <React.Fragment>
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <Card
+          style={{
+            background: "transparent",
+            border: "none",
+            margin: "0 2rem",
+          }}
+        >
+          <Card.Title
+            className="title"
+            style={{ fontSize: "30px" }}
+            dangerouslySetInnerHTML={{ __html: form.title.rendered }}
+          />
+          <Card.Body
+            className="body"
+            style={{ fontSize: "20px", padding: 0, marginTop: "2rem" }}
+            dangerouslySetInnerHTML={{ __html: form.content.rendered }}
+          />
+        </Card>
+      )}
+    </React.Fragment>
   );
 }
 export default ConsentForm;
