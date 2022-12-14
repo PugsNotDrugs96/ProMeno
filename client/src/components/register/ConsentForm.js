f47c862b6b66a573df2eb4912d50d9147b8e828d
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2022-12-14 19:56:34 +0100

    Made consent form work and added error message

--- client/src/components/register/ConsentForm.js
+++ client/src/components/register/ConsentForm.js
@@ -1,35 +1,44 @@
+import "./ConsentForm.css";
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { getPostBySlug } from "../../api/api";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 function ConsentForm() {
-  const [forms, setForms] = useState(null);
+  const [form, setForm] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const forms = await getPostBySlug("villkor-for-studien");
-      setForms(forms);
+      const form = await getPostBySlug("villkor-for-studien");
+      setForm(form);
     }
     fetchData();
   }, []);
 
-  if (!forms) return null;
+  if (!form)
+    return (
+      <Alert severity="error">
+        <AlertTitle>Sidan kunde inte hittas</AlertTitle>
+        Något gick fel, försök igen
+      </Alert>
+    );
 
   return (
-    <>
-      {forms.map((form, index) => (
-        <Card key={index} style={{ background: "transparent", border: "none" }}>
-          <Card.Title
-            style={{ fontSize: "30px" }}
-            dangerouslySetInnerHTML={{ __html: form.title.rendered }}
-          />
-          <Card.Body
-            style={{ fontSize: "20px", padding: 0, marginTop: "2rem" }}
-            dangerouslySetInnerHTML={{ __html: form.content.rendered }}
-          />
-        </Card>
-      ))}
-    </>
+    <Card
+      style={{ background: "transparent", border: "none", margin: "0 2rem" }}
+    >
+      <Card.Title
+        className="title"
+        style={{ fontSize: "30px" }}
+        dangerouslySetInnerHTML={{ __html: form.title.rendered }}
+      />
+      <Card.Body
+        className="body"
+        style={{ fontSize: "20px", padding: 0, marginTop: "2rem" }}
+        dangerouslySetInnerHTML={{ __html: form.content.rendered }}
+      />
+    </Card>
   );
 }
 export default ConsentForm;
