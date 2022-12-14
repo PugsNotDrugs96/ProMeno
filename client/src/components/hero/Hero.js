a9652b94bd24fc408984829652bbdf58f8b4e428
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2022-12-14 15:56:50 +0100

    Removed comments

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -52,37 +52,6 @@ function Hero() {
         </div>
       )}
     </Container>
-    /*
-    <div className="pt-5 text-center hero">
-      <h1 className="display-8 fw-bold">{title}</h1>
-      <h2 className="display-12">{subtitle}</h2>
-      {!user && (
-        <div className="col-lg-6 mx-auto">
-          <p className="lead mb-5 mt-4">
-            Forskningsstudien ProMeno handlar om levnadsvanor och hälsa hos
-            kvinnor i klimakteriet. Studien är en del av ett forskningsprojekt
-            vid Malmö Universitet där syftet är att utvärdera användningen av
-            ett digitalt hjälpmedel, mHealth, som stöd vid klimakteriebesvär hos
-            kvinnor i Sverige.
-          </p>
-          <p>
-            ProMeno är ett digitalt stöd i form av en applikation som är tänkt
-            att hjälpa kvinnor i klimakteriet att hitta relevant information om
-            symtom kopplade till klimakteriebesvär samt ge förslag på
-            behandling. Information kommer att samlas in genom formulär som
-            skickas ut med regelbunden intervall för att så småningom kunna
-            utvärdera effekten av användning.
-          </p>
-          <p>
-            Deltagarna kommer att delas in i tre grupper beroende på när de ska
-            få tillgång till applikationen. En grupp kommer få tillgång direkt,
-            en grupp kommer att få tillgång efter 3 månader och en efter 6
-            månader. Det kommer vara helt kostnadsfritt. Deltagare rekryteras på
-            promeno.se under januari-februari 2023.
-          </p>
-        </div>
-      )}
-    </div>*/
   );
 }
 
