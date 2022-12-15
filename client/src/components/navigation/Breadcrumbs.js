040d97805335a2f2832286f35e91917889523d08
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2022-12-15 20:10:08 +0100

    color and margin changes

--- client/src/components/navigation/Breadcrumbs.js
+++ client/src/components/navigation/Breadcrumbs.js
@@ -2,6 +2,7 @@ import React from "react";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Container from "react-bootstrap/Container";
 import { useLocation } from "react-router-dom";
+import "./Breadcrumbs.css";
 
 function Breadcrumbs() {
     const location = useLocation().pathname;
