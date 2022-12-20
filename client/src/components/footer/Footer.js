3d714853e8e79c4fbad6f7ffb839b4d8b4e7bac5
Qassem <46705041+Qassem97@users.noreply.github.com>
2022-12-20 08:53:09 +0100

    footer color

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,14 +3,16 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Contact from "./Contact";
 import Card from 'react-bootstrap/Card';
+import "./Footer.css"
 function Footer() {
   return (
-      <Container className="pt-1 pb-1">
+      <Container className="footer pt-1 pb-1">
             <h3 className="pb-3">Kontakt</h3>
         < Contact/>
         <Container>
-          <span>Läs mer </span>
-        <Card.Link href="/about-us">Om oss</Card.Link>
+          <h3 className="pb-3">Läs mer
+        <Card.Link href="/about-us"> om oss</Card.Link>
+          </h3>
         </Container>
         <Container className="pt-1 text-center">
           <small className="text-muted">
