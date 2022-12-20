73be6b4939ce51fc7f00761e49638f06ce4c75ab
Qassem <46705041+Qassem97@users.noreply.github.com>
2022-12-20 08:54:03 +0100

    mindre kontakt info

--- client/src/components/footer/Contact.js
+++ client/src/components/footer/Contact.js
@@ -1,6 +1,5 @@
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Figure from 'react-bootstrap/Figure';
 import EvaImg from "../../assets/logo.jpeg";
 import MarieImg from "../../assets/logo.jpeg";
@@ -8,7 +7,6 @@ import MarieImg from "../../assets/logo.jpeg";
 
 function Contact() {
 return (
-    
     <CardGroup>
     <Card style={{ alignItems: 'center', justifyContent: 'center'}}>
         <Card.Header as= "h5">Ansvarig forskare </Card.Header>
@@ -22,24 +20,17 @@ return (
         <Card.Body className='text-center'>
             <Card.Title >Eva</Card.Title>
             <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Some quick example text to build on the card title and make up.
             </Card.Text>
         </Card.Body>
-        <ListGroup className="list-group-flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
         <Card.Body>
-            <Card.Link href="#">Link</Card.Link>
+            {/* <Card.Link href="#">Link</Card.Link> */}
         </Card.Body>
     </Card>
     
     <Card style={{ alignItems: 'center', justifyContent: 'center'}}>
     <Card.Header  as= "h5">Ansvarig forskare </Card.Header>
     <Figure.Image
-        
         className='rounded-circle img-thumbnail'
         width={171}
         height={180}
@@ -49,16 +40,8 @@ return (
     <Card.Body className='text-center'>
         <Card.Title>Marie</Card.Title>
         <Card.Text>
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
+        Some quick example text to build on the card title and make up.
         </Card.Text>
-    </Card.Body>
-    <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-    </ListGroup>
-    <Card.Body>
     </Card.Body>
     </Card>
     </CardGroup>
