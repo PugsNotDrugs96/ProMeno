9e1c6abec8d523f65b4a868b10c2cdf5cc52b537
Qassem <46705041+Qassem97@users.noreply.github.com>
2022-12-22 14:44:16 +0100

    finputsa och fixa

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -2,7 +2,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Contact from "./Contact";
-import Card from 'react-bootstrap/Card';
 import "./Footer.css"
 function Footer() {
   return (
@@ -10,7 +9,6 @@ function Footer() {
       <Container>
         <h3>Kontakt</h3>
         < Contact/>
-        <h5>Läs mer<Card.Link href="/about-us"> Om oss</Card.Link></h5>
         <Container className="text-center">
           <small className="text-muted">
             © ProMeno, 2022. All rights reserved.
