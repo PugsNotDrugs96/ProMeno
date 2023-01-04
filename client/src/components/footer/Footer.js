6d233599750919979e90c0955c2b3e916fcc6658
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-01-04 15:21:03 +0100

    footer fix?

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -9,60 +9,59 @@ import EvaImg from "../../assets/eva.jpg";
 
 function Footer() {
   return (
-    <Container
-      className="footer"
-      style={{ alignItems: "center", justifyContent: "center" }}
-    >
-      <h3
-        className="text-center"
-        style={{ marginTop: "1rem", marginBottom: "2rem" }}
-      >
-        Ansvariga forskare
-      </h3>
-      <CardGroup>
-        {researchers.map((researcher, index) => (
-          <Card
-            key={index}
-            style={{
-              alignItems: "center",
-              justifyContent: "center",
-              border: "none",
-            }}
-          >
-            <Figure.Image
-              className="rounded-circle"
-              width={145}
-              alt="researcher image"
-              src={researcher.name === "Marie Karlsson" ? MarieImg : EvaImg}
-            />
-            <Card.Body>
-              <Card.Title
-                className="text-center"
-                style={{
-                  color: "#2b8578",
-                  fontWeight: "bold",
-                  fontSize: "25px",
-                }}
+    <div className="footer shadow">
+      <Container className="footerContainer" style={{ alignItems: "center", justifyContent: "center" }}>
+        <h3
+          className="text-center"
+          style={{ marginTop: "1rem", marginBottom: "2rem" }}
+        >
+          Ansvariga forskare
+        </h3>
+        <CardGroup>
+          {researchers.map((researcher, index) => (
+            <Card
+              key={index}
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                border: "none",
+              }}
+            >
+              <Figure.Image
+                className="rounded-circle"
+                width={145}
+                alt="researcher image"
                 src={researcher.name === "Marie Karlsson" ? MarieImg : EvaImg}
-              >
-                {researcher.name}
-              </Card.Title>
-              <>
-                {researcher.descriptions.map((description, index) => (
-                  <Card.Text
-                    className="text-center"
-                    style={{ marginTop: "1rem", color: "grey" }}
-                    key={index}
-                  >
-                    {description}
-                  </Card.Text>
-                ))}
-              </>
-            </Card.Body>
-          </Card>
-        ))}
-      </CardGroup>
-    </Container>
+              />
+              <Card.Body>
+                <Card.Title
+                  className="text-center"
+                  style={{
+                    color: "#2b8578",
+                    fontWeight: "bold",
+                    fontSize: "25px",
+                  }}
+                  src={researcher.name === "Marie Karlsson" ? MarieImg : EvaImg}
+                >
+                  {researcher.name}
+                </Card.Title>
+                <>
+                  {researcher.descriptions.map((description, index) => (
+                    <Card.Text
+                      className="text-center"
+                      style={{ marginTop: "1rem", color: "grey" }}
+                      key={index}
+                    >
+                      {description}
+                    </Card.Text>
+                  ))}
+                </>
+              </Card.Body>
+            </Card>
+          ))}
+        </CardGroup>
+      </Container>
+    </div>
   );
 }
 
